48b480f18fa6f302e7b35e1d81b236e01aa5cfe8
Friedel Wolff <friedel@translate.org.za>
2020-04-08 06:48:31 +0200

    Correctly indicate Cython language_level

--- fmmap.pyx
+++ fmmap.pyx
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# cython: language_level: 3
+# cython: language_level=3
 
 from mmap import *
 import sys
