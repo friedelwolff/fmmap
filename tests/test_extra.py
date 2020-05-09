39b874fc9aea4a5386d35bc57f0d8cd045e83129
Friedel Wolff <friedel@translate.org.za>
2020-05-09 15:12:55 +0200

    Use context manager for tests that didn't have .close()

--- tests/test_extra.py
+++ tests/test_extra.py
@@ -6,19 +6,19 @@ import fmmap as mmap
 
 
 def test_off_by_one():
-    m = mmap.mmap(-1, 10)
-    m.write(b"jj________")
-    m.seek(0)
-    assert m.find(b"jj") == 0
-    assert m.rfind(b"jj") == 0
+    with mmap.mmap(-1, 10) as m:
+        m.write(b"jj________")
+        m.seek(0)
+        assert m.find(b"jj") == 0
+        assert m.rfind(b"jj") == 0
 
 
 def test_needle_too_big():
-    m = mmap.mmap(-1, 10)
-    m.write(b"jj________")
-    m.seek(0)
-    assert m.find(b"abc", 9) == -1
-    assert m.rfind(b"abc", 9) == -1
+    with mmap.mmap(-1, 10) as m:
+        m.write(b"jj________")
+        m.seek(0)
+        assert m.find(b"abc", 9) == -1
+        assert m.rfind(b"abc", 9) == -1
 
 
 def test_clean_namespace():
